7791cbab1da2ecf3c542e64e5cbb58f77e1b14e5
pkoky <sscsk2224@gmail.com>
2021-11-28 09:28:13 +0900

    Create functions
    JavaScriptでベースとなる関数を作成しました。

--- src/main.js
+++ src/main.js
@@ -2,37 +2,184 @@ import fetch from 'node-fetch'
 
 
 const config = {
+    'URL': 'https://api.recursionist.io/builder/computers?type=',
     'CPU': {
         'Brand': [],
         'Model': [],
     },
     'GPU': [],
+    'STORAGE': ['ssd', 'hdd']
 
 }
 
-function selectCpu() {
+class Controller {
+    static getSelectedObj(arr, selectedModel) {
+        let resultObj = arr.find(ele => ele.Model === selectedModel);
+        return resultObj;
+    }
+}
 
+function selectCpu() {
+    fetch(config.URL + "cpu").then(res=>res.json()).then(function(data) {
+        let cpuMap = new Map();
     
-    fetch("https://api.recursionist.io/builder/computers?type=cpu").then(res=>res.json()).then(function(data) {
+        for (let curr of data) {
+            if (cpuMap.has(curr.Brand)) {
+                cpuMap.get(curr.Brand).push(curr)
+            } else {
+                cpuMap.set(curr.Brand, [curr]);
+            };
+        }
+        
+        // cpu連想配列からbrandを配列として抜き出す
+        let brandArr = Array.from(cpuMap.keys());
+
+        // Brandをセレクト
+        let selectedBrand = brandArr[1];
+
+        let cpuArrForSelectModel = cpuMap.get(selectedBrand);
+
+        let modelArr = [];
+        // セレクトしたブランドの配列からモデルを抜き出し
+        cpuArrForSelectModel.forEach(ele => modelArr.push(ele.Model));
+        
+        let selectedModel = modelArr[0];
+        // ブランドとモデルが一致したデータを抽出
+        let cpuObj = cpuArrForSelectModel.filter(ele => ele.Model === selectedModel)[0];
+        console.log(cpuObj)
+
+        
+        selectGpu()
+
+    })
+}
+
+function selectGpu() {
+    fetch(config.URL + "gpu").then(res=>res.json()).then(function(data) {
+        let gpuMap = new Map();
+
+        for (let curr of data) {
+            if (gpuMap.has(curr.Brand)) {
+                gpuMap.get(curr.Brand).push(curr)
+            } else {
+                gpuMap.set(curr.Brand, [curr]);
+            };
+        }
+        let brandArr = Array.from(gpuMap.keys());
+        
+        let selectedBrand = brandArr[0];
+
+        let gpuArrForSelectModel = gpuMap.get(selectedBrand)
+
+        let modelArr = [];
+        gpuArrForSelectModel.forEach(ele => modelArr.push(ele.Model));
+
+        let selectedModel = modelArr[0];
+
+        let gpuObj = gpuArrForSelectModel.filter(ele => ele.Model === selectedModel)[0];
+
+        console.log(gpuObj)
+
+        selectRam()
+    })
+}
+
+function selectRam() {
+    fetch(config.URL + "ram").then(response=>response.json()).then(function(data) {
+        let ramMap = new Map();
+
+        for (let curr of data) {
+            let spaceIndex = curr.Model.lastIndexOf(' ');
+            let xIndex = curr.Model.lastIndexOf('x');
+            let currNum = curr.Model.substring(spaceIndex+1, xIndex);
+            if (ramMap.has(currNum)) {
+                ramMap.get(currNum).push(curr)
+            } else {
+                ramMap.set(currNum, [curr]);
+            };
+        }
+
+        let numArr = Array.from(ramMap.keys());
+        let selectedNum = numArr[1];
+
         let brandSet = new Set();
-    
+
+        // ブランド抽出
+        ramMap.get(selectedNum).forEach(ele => brandSet.add(ele.Brand))
+
+        let brandArr = Array.from(brandSet);
+        let selectedBrand = brandArr[2];
+        let ramArrForSelectModel = ramMap.get(selectedNum).filter(ele => ele.Brand === selectedBrand);
+        let modelArr = [];
+        ramArrForSelectModel.forEach(ele => modelArr.push(ele.Model))
+
+        let selectedModel = modelArr[1];
+
+        let ramObj = ramArrForSelectModel.filter(ele => ele.Model === selectedModel)[0];
+
+        console.log(ramObj)
+
+        selectStorage();
+    })
+}
+
+
+function selectStorage() {
+    let selectedStorageType = config.STORAGE[0];
+    fetch(config.URL + selectedStorageType).then(response=>response.json()).then(function(data) {
+        let storageMap = new Map();
+
+
         for (let curr of data) {
-            brandSet.add(curr.Brand);
+            let bIndex = curr.Model.lastIndexOf('B');
+            let spaceIndex = curr.Model.lastIndexOf(' ', bIndex);
+            let currStorageCapacity = curr.Model.substring(spaceIndex+1, bIndex+1);
+            if (storageMap.has(currStorageCapacity)) {
+                storageMap.get(currStorageCapacity).push(curr)
+            } else {
+                storageMap.set(currStorageCapacity, [curr]);
+            };
         }
-    
-        let brands = Array.from(brandSet);
+
+        let storageArr = Array.from(storageMap.keys());
         
-        let selectedBrand = brands[1];
-        
-        let modelSet = new Set();
-        for (let curr of data) {
-            if (curr.Brand === selectedBrand) {
-                modelSet.add(curr.Model);
+        storageArr.sort(function(a,b) {
+            let aStorageType = a.substr(a.length-2, 1);
+            let bStorageType = b.substr(b.length-2, 1);
+            
+            let aStorageCapacity = a.substring(0, a.length-2) ;
+            let bStorageCapacity = b.substring(0, b.length-2) ;
+            
+            if (bStorageType === "T" && aStorageType != bStorageType) {
+                return -1;
+            } else if (aStorageType === "T" && aStorageType != bStorageType) {
+                return 1;
+            } else if (aStorageCapacity > bStorageCapacity) {
+                return 1;
+            } else {
+                return -1;
             }
-        }
-        let models = Array.from(modelSet);
-        let selectedModel = models[0];
-        console.log(selectedModel)
+        })
+
+        let selectedStorageCapacity = storageArr[2];
+
+
+        let brandSet = new Set();
+
+        // ブランド抽出
+        storageMap.get(selectedStorageCapacity).forEach(ele => brandSet.add(ele.Brand));
+        let brandArr = Array.from(brandSet);
+        let selectedBrand = brandArr[0];
+
+
+        let storageArrForSelectModel = storageMap.get(selectedStorageCapacity).filter(ele => ele.Brand === selectedBrand);
+
+        let modelArr = [];
+        storageArrForSelectModel.forEach(ele => modelArr.push(ele.Model));
+
+        let selectedModel = modelArr[0];
+        let storageObj = storageArrForSelectModel.filter(ele => ele.Model === selectedModel)[0];
+        console.log(storageObj)
     })
 }
 
@@ -46,3 +193,4 @@ selectCpu()
 //       message: 'Hello Vue!'
 //     }
 // })
+
